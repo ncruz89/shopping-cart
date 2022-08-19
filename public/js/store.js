1f5e3cad0628833958fdc61712e6f59f60618cd9
ncruz89 <nathan.scruggs.cruz@gmail.com>
2022-08-19 11:25:21 -0400

    added console.log with stripe card test numbers

--- public/js/store.js
+++ public/js/store.js
@@ -188,6 +188,10 @@ const init = function () {
   addRemoveBtn();
   quantityChange();
   addPurchaseBtn();
+  console.log(
+    "Can test stripe with card numbers: 4242 4242 4242 4242 for payment accepted."
+  );
+  console.log("4000 0000 0000 0002 for payment declined");
 };
 
 init();
